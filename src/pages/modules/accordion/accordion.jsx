af2ce13ba5f0766a7970bd41b3b830bd826c4828
Cris0338 <cris0338@gmail.com>
2024-02-10 04:56:25 +0100

    About jsx & scss : ok

--- src/pages/modules/accordion/accordion.jsx
+++ src/pages/modules/accordion/accordion.jsx
@@ -1,20 +1,24 @@
 import "./accordion.scss";
 import React, { useState } from "react";
+import arrow from '/arrow_24px.png';
 
 function Accordion({ title, content }) {
   const [isActive, setIsActive] = useState(false);
 
+  const handleClick = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div className="accordion-container">
-      <div className={`accordion-item ${isActive ? "active" : ""}`}>
-        <div
-          className="accordion-title"
-          onClick={() => setIsActive(!isActive)}
-        >
+      <div className={`accordion-item ${isActive ? "action" : ""}`}>
+        <div className="accordion-title" onClick={handleClick}>
           <h2>{title}</h2>
-          <div className="accordion-icon">{isActive ? "-" : "+"}</div>
+          <div className="accordion-icon">
+            <img src={arrow} alt="Toggle" />
+          </div>
         </div>
-        <div className={`accordion-content ${isActive ? "active" : ""}`}>{content}</div>
+        <div className={`accordion-content ${isActive ? "action" : ""}`}>{content}</div>
       </div>
     </div>
   );
