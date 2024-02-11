1c09fcb5b2a59a72fc1cf20671352c5e8715fdf5
Cris0338 <cris0338@gmail.com>
2024-02-11 16:57:39 +0100

    Undo vite.config.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
-// vite.config.js
+// vite.config.jsx
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/P8-Kasa-Vite/'
-});
-
+})
