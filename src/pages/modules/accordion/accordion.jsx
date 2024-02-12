9ad703d326d605e78dc51afd1e2751f417c4c969
Cris0338 <cris0338@gmail.com>
2024-02-12 21:44:22 +0100

    Fix click-element on accordion

--- src/pages/modules/accordion/accordion.jsx
+++ src/pages/modules/accordion/accordion.jsx
@@ -16,9 +16,9 @@ function Accordion({ title, content }) {
   return (
     <div className="accordion-container">
       <div className={`accordion-item ${isActive ? "action" : ""}`}>
-        <div className="accordion-title" onClick={handleClick}>
+        <div className="accordion-title-bar">
           <h2>{title}</h2>
-          <div className="accordion-icon">
+          <div className="accordion-icon" onClick={handleClick}>
             <FontAwesomeIcon icon={faAngleUp} />
             {/* <img src={arrow} alt="Toggle" /> */}
           </div>
